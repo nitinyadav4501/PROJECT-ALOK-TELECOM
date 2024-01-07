f52195c573d224de1f475dcd4bdacb1d5a2b7ff8
Nitin yadav <nitinyadavtrinet@gmail.com>
2024-01-08 02:50:26 +0530

    modify the website

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ sabkuchh.addEventListener("click",()=>{
     document.body.classList.toggle("lightTheme");
 })
 
+let hamBurger = document.querySelector(".hamBurger")
+hamBurger.addEventListener("click",()=>{
+    document.querySelector(".hamItems").style.transform = "translateX(0px)"
+})
+ 
+let cross = document.querySelector(".cross")
+cross.addEventListener("click",()=>{
+    document.querySelector(".hamItems").style.transform = "translateX(-1000px)"
+})
+
