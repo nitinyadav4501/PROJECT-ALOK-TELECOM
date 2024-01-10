76b1d10d1d8c1cef80e24c7a5183f7433e5b52b0
Nitin yadav <nitinyadavtrinet@gmail.com>
2024-01-10 18:53:36 +0530

    Fix some issues

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-let sabkuchh = document.querySelector(".dark i");
+let mode = document.querySelector(".dark i");
 
-sabkuchh.addEventListener("click",()=>{
+mode.addEventListener("click", () => {
     document.body.classList.toggle("lightTheme");
 })
 
+// Hamburger **************************************
 let hamBurger = document.querySelector(".hamBurger")
-hamBurger.addEventListener("click",()=>{
+hamBurger.addEventListener("click", () => {
     document.querySelector(".hamItems").style.transform = "translateX(0px)"
 })
- 
+
 let cross = document.querySelector(".cross")
-cross.addEventListener("click",()=>{
+cross.addEventListener("click", () => {
     document.querySelector(".hamItems").style.transform = "translateX(-1000px)"
 })
+
+
+// search for phone ************************************
+let glassForPhone = document.querySelector(".glassForPhone")
+let show = true;
+glassForPhone.addEventListener("click", () => {
+    if (show === true) {
+        document.querySelector(".searchPhone input").style.transform = "scale(1)";
+        show = false
+    }
+    else {
+        document.querySelector(".searchPhone input").style.transform = "scale(0)";
+        show = true
+    }
+
+})
+
 
